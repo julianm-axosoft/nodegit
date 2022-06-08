865d557f2d66fce8c6d255968d58f115deb4c81d
Julian Mesa <julian.mesa@gitkraken.com>
2022-06-08 20:44:23 +0200

    Fix memory corruption

--- generate/templates/manual/tree/get_all_entries.cc
+++ generate/templates/manual/tree/get_all_entries.cc
@@ -1,22 +1,30 @@
 
 struct _HandleParentTreeEntry {
-    v8::Local<v8::Value> treeObj;
+    git_tree * treeObj;
     const git_tree_entry * entry;
 };
 
-void _IterateTree(git_repository *repo, git_tree *tree, std::vector<_HandleParentTreeEntry> &entries) {
+void _IterateTree(git_repository *repo, git_tree *tree, std::vector<_HandleParentTreeEntry> &entries,
+    std::unordered_set<const git_tree_entry *> &visited) {
+
   size_t size = git_tree_entrycount(tree);
-  v8::Local<v8::Value> treeObj = GitTree::New(tree, true);
   for (size_t i = 0; i < size; i++) {
     const git_tree_entry *entry = git_tree_entry_byindex(tree, i);
     auto filemode = git_tree_entry_filemode(entry);
-    if (filemode == GIT_FILEMODE_BLOB || filemode == GIT_FILEMODE_BLOB_EXECUTABLE) {
-      entries.push_back({treeObj, entry});
-    }
-    if (filemode == GIT_FILEMODE_TREE) {
-      git_tree *subtree;
-      git_tree_lookup(&subtree, repo, git_tree_entry_id(entry));
-      _IterateTree(repo, subtree, entries);
+    if (visited.find(entry) == visited.end()) {
+        visited.insert(entry);
+
+        if (filemode == GIT_FILEMODE_BLOB || filemode == GIT_FILEMODE_BLOB_EXECUTABLE) {
+          if (visited.find(entry) == visited.end()) {
+            entries.push_back({tree, entry});
+            visited.insert(entry);
+          }
+        }
+        if (filemode == GIT_FILEMODE_TREE) {
+          git_tree *subtree;
+          git_tree_lookup(&subtree, repo, git_tree_entry_id(entry));
+          _IterateTree(repo, subtree, entries, visited);
+        }
     }
   }
 }
@@ -34,17 +42,20 @@ NAN_METHOD(GitTree::GetAllEntries) {
   git_repository * repo = Nan::ObjectWrap::Unwrap<GitRepository>(Nan::To<v8::Object>(info[0]).ToLocalChecked())->GetValue();
 
   std::vector<_HandleParentTreeEntry> entries;
+  std::unordered_set<const git_tree_entry *> visited;
 
-  _IterateTree(repo, tree, entries);
+  _IterateTree(repo, tree, entries, visited);
 
   Local<Array> result = Nan::New<Array>(entries.size());
 
+  v8::Local<v8::Context> context = Nan::GetCurrentContext();
   for (size_t i = 0; i < entries.size(); i++) {
     v8::Local<v8::Value> entryValue = GitTreeEntry::New(entries[i].entry, true);
+    v8::Local<v8::Value> treeObj = GitTree::New(entries[i].treeObj, true);
+
     const v8::Local<v8::Object> entryObj = entryValue.As<v8::Object>();
 
-    v8::Local<v8::Context> context = Nan::GetCurrentContext();
-    if (entryObj->Set(context, Nan::New<v8::String>("parent").ToLocalChecked(), entries[i].treeObj).IsJust() == false) {
+    if (entryObj->Set(context, Nan::New<v8::String>("parent").ToLocalChecked(), treeObj).IsJust() == false) {
       return Nan::ThrowError("Could not set property parent");
     }
 
